440cb4ba0f8dd97a6a824e03f8e2eb7a056e9783
jiwoongj <jiwoongj@andrew.cmu.edu>
2023-01-10 14:00:42 +0900

    replacing pt 2

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,10 @@
 const headerNavLinks = [
+  { href: '/about', title: 'About' },
+  { href: '/research', title: 'Research' },
   { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-  { href: '/now', title: 'Now' },
+
+  // { href: '/now', title: 'Now' },
   // { href: '/my-books', title: 'LaTeX book' },
   // { href: '/latex-template', title: 'Latex' },
 ]
