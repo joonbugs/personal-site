187ae4b777ad7a3fa49b8a29c7c9d3b89a6dd4dc
jiwoongj <jiwoongj@andrew.cmu.edu>
2023-01-10 14:38:18 +0900

    prettier formatting: :

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,10 +1,10 @@
-import SocialIcon from '@/components/social-icons'
-import Image from '@/components/Image'
-import Link from '@/components/Link'
-import { PageSEO } from '@/components/SEO'
-import Experience from '@/components/Experience'
-import experienceData from '@/data/experienceData'
-import { RoughNotation } from 'react-rough-notation'
+import SocialIcon from '@/components/social-icons';
+import Image from '@/components/Image';
+import Link from '@/components/Link';
+import { PageSEO } from '@/components/SEO';
+import Experience from '@/components/Experience';
+import experienceData from '@/data/experienceData';
+import { RoughNotation } from 'react-rough-notation';
 
 export default function AuthorLayout({ children, frontMatter }) {
   const {
@@ -19,47 +19,49 @@ export default function AuthorLayout({ children, frontMatter }) {
     text1,
     text2,
     text3,
-  } = frontMatter
+  } = frontMatter;
 
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
-      <div className="">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+      <div className=''>
+        <div className='pt-6 pb-8 space-y-2 md:space-y-5'>
+          <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
             About
           </h1>
         </div>
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8 space-x-2">
+        <div className='items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0'>
+          <div className='flex flex-col items-center pt-8 space-x-2'>
             <Image
               src={avatar}
-              alt="avatar"
-              width="192px"
-              height="192px"
-              className="w-48 h-48 rounded-full"
+              alt='avatar'
+              width='192px'
+              height='192px'
+              className='w-48 h-48 rounded-full'
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <h3 className='pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight'>
+              {name}
+            </h3>
+            <div className='text-gray-500 dark:text-gray-400'>{occupation}</div>
+            <div className='text-gray-500 dark:text-gray-400'>{company}</div>
           </div>
           {/* <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">{children}</div> */}
-          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
+          <div className='pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2'>
             <p>{text1}</p>
             <span>Are you interested in what I'm doing </span>
             <Link
               href={'/now'}
-              className="special-underline hover:dark:text-gray-800 dark:text-gray-100 no-underline"
+              className='special-underline hover:dark:text-gray-800 dark:text-gray-100 no-underline'
             >
               now{' '}
             </Link>
             <br />
             <p>
               <RoughNotation
-                type="bracket"
+                type='bracket'
                 brackets={['left', 'right']}
                 show={true}
-                color="#FF0000"
+                color='#FF0000'
               >
                 {text2}
               </RoughNotation>
@@ -69,7 +71,7 @@ export default function AuthorLayout({ children, frontMatter }) {
               {text3} <br />
               <Link
                 href={'/uses'}
-                className="special-underline no-underline hover:dark:text-gray-800 dark:text-gray-100"
+                className='special-underline no-underline hover:dark:text-gray-800 dark:text-gray-100'
               >
                 Here{' '}
               </Link>
@@ -77,21 +79,29 @@ export default function AuthorLayout({ children, frontMatter }) {
             </p>
             <br />
             <p>
-              Feel free to reach out if you have anything to talk about, you can reach me on{' '}
-              <RoughNotation type="highlight" show={true} color="#FBCFE8" animationDelay={1200}>
-                <span className="dark:text-gray-700">einargudnig@gmail.com </span>
+              Feel free to reach out if you have anything to talk about, you can
+              reach me on{' '}
+              <RoughNotation
+                type='highlight'
+                show={true}
+                color='#FBCFE8'
+                animationDelay={1200}
+              >
+                <span className='dark:text-gray-700'>
+                  einargudnig@gmail.com{' '}
+                </span>
               </RoughNotation>
               or on social media.
             </p>
           </div>
         </div>
-        <div className="mt-10">
-          <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className='mt-10'>
+          <div className='pt-6 pb-8 space-y-2 md:space-y-5'>
+            <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
               Experience
             </h1>
           </div>
-          <div className="pt-8 pb-8 max-w-none xl:col-span-2">
+          <div className='pt-8 pb-8 max-w-none xl:col-span-2'>
             {experienceData.map((d) => (
               <Experience
                 key={d.company}
@@ -109,5 +119,5 @@ export default function AuthorLayout({ children, frontMatter }) {
         </div>
       </div>
     </>
-  )
+  );
 }
