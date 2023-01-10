6b5406fed610ee99afde9a5aa45f9865e1da17f0
jiwoongj <jiwoongj@andrew.cmu.edu>
2023-01-10 14:09:22 +0900

    more changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,75 +1,78 @@
 const siteMetadata = {
-  title: 'Hi!👋',
-  author: 'Einar Guðni Guðjónsson',
+  title: 'Home | About Joon',
+  author: 'Joon Jang',
   headerTitle: 'My website',
-  description: 'My online place for thoughts',
+  description: 'testing 123',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://einargudni.com,',
-  siteRepo: 'https://github.com/einargudnig/personal-blog-v3',
+  siteUrl: 'https://joonbugs.github.io,',
+  siteRepo: 'https://github.com/joonbugs', // update soon!
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/einar-avatar.jpg',
+  profile: '/static/images/profile/cafe-shot-1.jpg',
+  image: '/static/images/profile/cafe-shot-1.jpg',
   socialBanner: '/static/images/twittercard.png',
-  email: 'einargudnig@gmail.com',
-  github: 'https://github.com/einargudnig',
-  twitter: 'https://twitter.com/einargudni',
-  facebook: 'https://facebook.com',
-  instagram: 'https://instagram.com/einargudni',
-  linkedin: 'https://www.linkedin.com/einargudni/',
+  email: 'jiwoongj@cs.cmu.edu',
+  github: 'https://github.com/joonbugs',
+  gscholar: 'https://scholar.google.com/citations?user=JqGqynUAAAAJ&hl=en',
+  // twitter: 'https://twitter.com/einargudni',
+  // facebook: 'https://facebook.com',
+  // instagram: 'https://instagram.com/einargudni',
+  linkedin: '',
+  //fill in affiliations
   locale: 'en-US',
-  analytics: {
-    // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    googleAnalyticsId: 'G-95HPC8T3GB', // e.g. UA-000000-2 or G-XXXXXXX
-  },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
-  comment: {
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: '', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-    },
-    utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
-      // theme when dark mode
-      darkTheme: '',
-    },
-    disqusConfig: {
-      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-    },
-  },
+  // analytics: {
+  //   // supports plausible, simpleAnalytics or googleAnalytics
+  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+  //   simpleAnalytics: false, // true or false
+  //   googleAnalyticsId: 'GA-ID', // e.g. UA-000000-2 or G-XXXXXXX
+  // },
+  // newsletter: {
+  //   // supports mailchimp, buttondown, convertkit, klaviyo
+  //   // Please add your .env file and modify it according to your selection
+  //   provider: 'buttondown',
+  // },
+  // comment: {
+  //   // Select a provider and use the environment variables associated to it
+  //   // https://vercel.com/docs/environment-variables
+  //   provider: '', // supported providers: giscus, utterances, disqus
+  //   giscusConfig: {
+  //     // Visit the link below, and follow the steps in the 'configuration' section
+  //     // https://giscus.app/
+  //     repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+  //     repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+  //     category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+  //     categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+  //     mapping: 'pathname', // supported options: pathname, url, title
+  //     reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+  //     // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+  //     metadata: '0',
+  //     // theme example: light, dark, dark_dimmed, dark_high_contrast
+  //     // transparent_dark, preferred_color_scheme, custom
+  //     theme: 'light',
+  //     // theme when dark mode
+  //     darkTheme: 'transparent_dark',
+  //     // If the theme option above is set to 'custom`
+  //     // please provide a link below to your custom theme css file.
+  //     // example: https://giscus.app/themes/custom_example.css
+  //     themeURL: '',
+  //   },
+  //   utterancesConfig: {
+  //     // Visit the link below, and follow the steps in the 'configuration' section
+  //     // https://utteranc.es/
+  //     repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+  //     issueTerm: '', // supported options: pathname, url, title
+  //     label: '', // label (optional): Comment 💬
+  //     // theme example: github-light, github-dark, preferred-color-scheme
+  //     // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+  //     theme: '',
+  //     // theme when dark mode
+  //     darkTheme: '',
+  //   },
+  //   disqusConfig: {
+  //     // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+  //     shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+  //   },
+  // },
 }
 
 module.exports = siteMetadata
